b38612441ffb65c0c8dddc3e9611e85ce5d7459e
Charlotte Evans <cie1@rice.edu>
2018-01-29 16:54:32 -0600

    created thermovoltage GUI build

--- optics/thermovoltage_measurement/main_scan_gui.py
+++ optics/thermovoltage_measurement/main_scan_gui.py
@@ -1,6 +1,6 @@
 from optics.hardware_control import sr7270, npc3sg_analog, hardware_addresses_and_constants
 import tkinter as tk
-from optics.gui.gui_builds import ThermovoltageGUI
+from optics.gui.gui_builds import SetupGUI
 
 if __name__ == '__main__':
     root = tk.Tk()
@@ -15,8 +15,8 @@ if __name__ == '__main__':
             sr7270.create_endpoints(hardware_addresses_and_constants.vendor, hardware_addresses_and_constants.product) \
                     as (sr7270_top, sr7270_bottom):
         try:
-            thermovoltage_scan_gui = ThermovoltageGUI(root, title, fields, npc3sg_x, npc3sg_y, npc3sg_input,
-                                                      sr7270_top, sr7270_bottom)
+            thermovoltage_scan_gui = SetupGUI(root, title, fields, npc3sg_x, npc3sg_y, npc3sg_input,
+                                              sr7270_top, sr7270_bottom)
             thermovoltage_scan_gui.build_thermovoltage_scan_gui()
             root.mainloop()
         except KeyboardInterrupt:
