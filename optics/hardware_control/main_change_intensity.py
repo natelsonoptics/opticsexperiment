a3b6460f588a708a9e47a45b15bc8be39d3b2de5
Charlotte Evans <cie1@rice.edu>
2018-01-30 14:13:42 -0600

    created GUI to change intensity

---
+++ optics/hardware_control/main_change_intensity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import tkinter as tk
+from optics.hardware_control import attenuator_wheel, pm100d, hardware_addresses_and_constants
+from optics.gui.gui_builds import SetupGUI
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    fields = {'steps': 10}
+    with pm100d.connect(hardware_addresses_and_constants.pm100d_address) as q, attenuator_wheel.create_do_task(hardware_addresses_and_constants.stepper_outputs) as r:
+        my_gui = SetupGUI(root, 'Change laser power', fields, attenuatorwheel=r, powermeter=q)
+        my_gui.build_intensity_gui()
+        root.mainloop()
