4b3223a66f3e016bb0f69e6997955a4eb90cd29a
Charlotte Evans <cie1@rice.edu>
2018-01-30 14:25:12 -0600

    created intensity change GUI

--- optics/gui/gui_builds.py
+++ optics/gui/gui_builds.py
@@ -5,18 +5,20 @@ import tkinter.filedialog
 from optics.thermovoltage_measurement.thermovoltage_scan import ThermovoltageScan
 from optics.heating_measurement.heating_scan import HeatingScan
 from optics.thermovoltage_measurement.thermovoltage_time import ThermovoltageTime
+from optics.hardware_control.change_intensity import ChangeIntensity
 
 
 class SetupGUI:
     def __init__(self, master, caption, fields, npc3sg_x=None, npc3sg_y=None, npc3sg_input=None,
-                 sr7270_top=None, sr7270_bottom=None):
+                 sr7270_top=None, sr7270_bottom=None, powermeter=None, attenuatorwheel=None):
         self.master = master
         self.master.title(caption)
         self.fields = fields
         self.entries = []
         self.inputs = {}
         self.filepath = tk.StringVar()  # this is a global variable - this needs to change
-        self.filepath.set(self.fields['file path'])
+        if 'file path' in self.fields:
+            self.filepath.set(self.fields['file path'])
         self.label = tk.Label(master, text=caption)
         self.label.pack()
         self.browse_button = tk.Button(self.master, text="Browse", command=self.onclick_browse)
@@ -25,6 +27,9 @@ class SetupGUI:
         self.npc3sg_input = npc3sg_input
         self.sr7270_top = sr7270_top
         self.sr7270_bottom = sr7270_bottom
+        self.powermeter = powermeter
+        self.attenuatorwheel = attenuatorwheel
+        self.textbox = None
 
     def makeform(self):
         for key in self.fields:
@@ -41,7 +46,7 @@ class SetupGUI:
             self.entries.append((key, ent))
         return self.entries
 
-    def fetch(self, event=None):
+    def fetch(self, event=None):  # stop trying to make fetch happen. It's not going to happen. -Regina, Mean Girls
         # You need to have "event" input even though it isn't used as an input. If this method was only fetch(self), it
         # only expects one input parameter, which would be fine except you also pass self.fetch to self.master.bind when
         # binding the <Return> key, which means that self.fetch('<Return>') is being called, which is TWO inputs due to
@@ -58,7 +63,7 @@ class SetupGUI:
                                 int(self.inputs['y pixel density']), int(self.inputs['x range']),
                                 int(self.inputs['y range']), int(self.inputs['x center']), int(self.inputs['y center']),
                                 float(self.inputs['polarization']), self.npc3sg_x, self.npc3sg_y, self.npc3sg_input,
-                                self.sr7270_top, self.sr7270_bottom)
+                                self.sr7270_top, self.sr7270_bottom, self.powermeter)
         run.main()
 
     def heating_scan(self, event=None):
@@ -69,7 +74,7 @@ class SetupGUI:
                           int(self.inputs['y pixel density']), int(self.inputs['x range']),
                           int(self.inputs['y range']), int(self.inputs['x center']), int(self.inputs['y center']),
                           float(self.inputs['polarization']), self.npc3sg_x, self.npc3sg_y, self.npc3sg_input,
-                          self.sr7270_top, self.sr7270_bottom)
+                          self.sr7270_top, self.sr7270_bottom, self.powermeter)
         run.main()
 
     def thermovoltage_time(self, event=None):
@@ -77,7 +82,8 @@ class SetupGUI:
         run = ThermovoltageTime(self.inputs['file path'], self.inputs['notes'], self.inputs['device'],
                                 int(self.inputs['scan']), float(self.inputs['gain']),
                                 float(self.inputs['rate (per second)']), float(self.inputs['max time (s)']),
-                                float(self.inputs['polarization']), self.npc3sg_input, self.sr7270_bottom)
+                                float(self.inputs['polarization']), self.npc3sg_input, self.sr7270_bottom,
+                                self.powermeter)
         run.main()
 
     def onclick_browse(self):
@@ -109,6 +115,36 @@ class SetupGUI:
         b1.pack(side=tk.LEFT, padx=5, pady=5)
         b2 = tk.Button(self.master, text='Quit', command=self.master.quit)
         b2.pack(side=tk.LEFT, padx=5, pady=5)
+
+    def step(self, direction):
+        self.fetch()
+        run = ChangeIntensity(self.attenuatorwheel, self.powermeter)
+        run.step(int(self.inputs['steps']), direction=direction)
+        self.power()
+
+    def power(self):
+        self.textbox.delete(1.0, tk.END)
+        run = ChangeIntensity(self.attenuatorwheel, self.powermeter)
+        self.textbox.insert(tk.END, run.read_power()*1000)
+        self.textbox.pack()
+
+    def build_intensity_gui(self):
+        self.makeform()
+        row = tk.Frame(self.master)
+        lab = tk.Label(row, width=15, text='Power (mW)', anchor='w')
+        row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+        lab.pack(side=tk.LEFT)
+        self.textbox = tk.Text(row, height=1, width=10)
+        self.textbox.insert(tk.END, str('None'))
+        self.textbox.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+        b1 = tk.Button(self.master, text='Forward', command=lambda: self.step(True))
+        b1.pack(side=tk.LEFT, padx=5, pady=5)
+        b2 = tk.Button(self.master, text='Backward', command=lambda: self.step(False))
+        b2.pack(side=tk.LEFT, padx=5, pady=5)
+        b3 = tk.Button(self.master, text='Read power', command=self.power)
+        b3.pack(side=tk.LEFT, padx=5, pady=5)
+        b4 = tk.Button(self.master, text='Quit', command=self.master.quit)
+        b4.pack(side=tk.LEFT, padx=5, pady=5)
 
 
 
