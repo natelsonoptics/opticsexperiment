9f25bba120b88f079202e7680c16ceee0570a625
charlotteievans <cie1@rice.edu>
2018-01-29 18:50:52 -0600

    changes made

--- optics/thermovoltage_plot/thermovoltage_plot.py
+++ optics/thermovoltage_plot/thermovoltage_plot.py
@@ -18,3 +18,37 @@ def plot(ax, im, voltage, max_val, min_val, plotlabel=None):
     norm = MidpointNormalize(midpoint=0, vmin=min_val, vmax=max_val)
     ax.imshow(voltage.T, norm=norm, cmap=plt.cm.coolwarm, interpolation='none', vmax=max_val, vmin=min_val, origin='lower')
     im.set_clim(min_val, max_val)
+
+
+class ColorMapPlot:
+    def __init__(self, fig, ax, data, norm=None, min_val=None, max_val=None, xlabel='x pixel', ylabel='y pixel',
+                 plotlabel='', colorbar_label='', origin='lower'):
+        self.fig = fig
+        self.ax = ax
+        self.data = data
+        self.norm = norm
+        self.min_val = min_val
+        self.max_val = max_val
+        self.xlabel = xlabel
+        self.ylabel = ylabel
+        self.plotlabel = plotlabel
+        self.colorbar_label = colorbar_label
+        self.origin = origin
+        self.im = self.ax.imshow(self.data.T, norm=self.norm, cmap=plt.cm.coolwarm, interpolation='nearest',
+                                 origin=self.origin)
+        self.im.set_clim(vmin=self.min_val)
+        self.im.set_clim(vmax=self.max_val)
+        self.clb = self.fig.colorbar(self.im, orientation='vertical', ax=self.ax)
+        self.clb.set_label(self.colorbar_label, rotation=270, labelpad=20)
+        self.ax.title.set_text(self.plotlabel)
+
+    def show(self):
+        self.fig.show()
+
+
+
+
+
+
+
+
