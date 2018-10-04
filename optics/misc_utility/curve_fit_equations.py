d7e8d7a1f1f073fc00ec1b53bde4976b3e7b9019
Charlotte Evans <cie1@rice.edu>
2018-10-03 19:48:44 -0500

    added cut throughs to thermovoltage scan

--- optics/misc_utility/curve_fit_equations.py
+++ optics/misc_utility/curve_fit_equations.py
@@ -10,4 +10,10 @@ def linear_fit(xdata, ydata):
     m_error, b_error = np.diag(pcov)
     return m, b, m_error, b_error
 
+def proprotional(x, m):
+    return m * x
+
+def proprortional_fit(xdata, ydata):
+    m, pcov = curve_fit(proprotional, xdata, ydata)
+    return m[0], pcov[0]
 
